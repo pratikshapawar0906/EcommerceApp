d67a0dc477b0a3b48a0e58b381ca8bff93612678
Pratiksha Pawar <pratikshapawar920@gmail.com>
2026-01-29 12:23:26 +0530

    Remove broken submodule

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,11 +1,13 @@
 import axios from "axios";
+const apiUrl=import.meta.env.VITE_API_URL;
+
 
 export const postData=async(url, formData)=>{
   try {
-    const response=await fetch(process.env.REACT_APP_API_URL + url,{
+    const response=await fetch(apiUrl + url,{
         method:'POST',
         headers:{
-            'Authorization':`Bearer ${token}`,
+            'Authorization':`Bearer ${localStorage.getItem("token")}`,
             'Content-Type':'application/json'
         },
         body:JSON.stringify(formData)
